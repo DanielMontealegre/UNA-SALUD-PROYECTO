6e4c2cf304fef189c350cb2cad69c7e252582bd6
Andrey <andrey_came@hotmail.com>
2017-05-18 20:15:34 -0600

    PresionArterial - casi lista

---
+++ UNA-SALUD/Controllers/PresionArterialController.cs
@@ -0,0 +1,63 @@
+﻿using ModeloDatos.Database;
+using ModeloDatos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UNA_SALUD.Models;
+
+namespace UNA_SALUD.Controllers
+{
+    public class PresionArterialController : Controller
+    {
+        // GET: PresionArterial
+        public ActionResult Index(string id)
+        {
+            var pacienteDB = pacientes.Obtener(id);
+            var pacienteModelo = new PacienteRegistro(pacienteDB);
+            ViewBag.Paciente = pacienteModelo;
+            return View();
+        }
+
+        public JsonResult GetPresionesArteriales(string id)
+        {
+            if (id != null)
+            {
+                var listaPresionesBD = presionesArteriales.Obtener(id);
+                var listaPresionesModelos = listaPresionesBD.Select(x => new PresionArterialModelo(x));
+                return Json(listaPresionesModelos, JsonRequestBehavior.AllowGet);
+            }
+            return Json(true);
+        }
+
+        public JsonResult AgregarPresionArterial(PresionArterial presionArterial)
+        {
+            var presionDB = presionesArteriales.Agregar(presionArterial);
+            return Json(new { Resultado = true, PresionArterial = new PresionArterialModelo(presionDB) });
+        }
+
+        public JsonResult ObtenerPresionArterial(int codigo)
+        {
+            PresionArterial presionDB = presionesArteriales.ObtenerPresionArterial(codigo);
+            return Json(new PresionArterialModelo(presionDB), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EditarPresionArterial(PresionArterial presionArterial)
+        {
+            var presionEditadoDb = presionesArteriales.EditarPresionArterial(presionArterial);
+            return Json(new { Resultado = true, PresionArterial = new PresionArterialModelo(presionEditadoDb) });
+        }
+
+        public JsonResult RemoverPresionArterial(int codigo)
+        {
+            presionesArteriales.Remover(codigo);
+            return Json(true);
+        }
+
+        private ConjuntoPresionesArteriales presionesArteriales = new ConjuntoPresionesArteriales();
+        private ConjuntoPacientes pacientes = new ConjuntoPacientes();
+
+    }
+}
---
+++ UNA-SALUD/Models/PresionArterialModelo.cs
@@ -0,0 +1,54 @@
+﻿using ModeloDatos.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UNA_SALUD.Models
+{
+    public class PresionArterialModelo
+    {
+        public long Codigo { get; set; }
+
+        [Required]
+        public string Paciente { get; set; }
+
+        [Required]
+        [Display(Name = "Sistolica(mmHg)")]
+        public double Sistolica { get; set; }
+
+        [Required]
+        [Display(Name = "Diastolica(mmHg)")]
+        public double Diastolica { get; set; }
+
+        [Required]
+        [Display(Name = "Pulso(Ipm)")]
+        public double Pulso { get; set; }
+
+        [Required]
+        [Display(Name = "Ritmo Irregular")]
+        public bool RitmoIrregular { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Fecha { get; set; }
+
+
+
+        [DataType(DataType.MultilineText)]
+        public string Detalle { get; set; }
+
+        public PresionArterialModelo(PresionArterial presion)
+        {
+            Codigo = presion.Codigo;
+            Paciente = presion.Paciente;
+            Fecha = presion.Fecha;
+            Sistolica = presion.Sistolica;
+            Diastolica = presion.Diastolica;
+            Pulso = presion.Pulso;
+            RitmoIrregular = presion.RitmoIrregular;
+            Detalle = presion.Detalle;
+        }
+    }
+}
